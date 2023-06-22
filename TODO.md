63d13cfd2086802d37c12d057e16f61e54edec61
Robert Gabriel <ephemeric@icloud.net>
2023-06-22 15:08:29 +0200

    Notes et al.

---
+++ TODO.md
@@ -0,0 +1,1 @@
+pipes: curl: (35) error:0A0000D9:SSL routines::unsolicited extension
--- useradd.sh
+++ useradd.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 # TODO: determine OS... do relevent tasks.
 #dnf -y install zsh vim policycoreutils-python policycoreutils nmap telnet wget curl tcpdump
 dpkg-query --show zsh || apt-get -y install zsh
 
-getent group wheel || groupadd -r wheel
-
 # Portable for Rocky, Ubuntu, et al.
-getent passwd robertg || useradd -c "Robert Gabriel" -m -s /bin/zsh -G wheel robertg
+#getent group wheel || groupadd -r wheel
+getent passwd robertg || useradd -c "Robert Gabriel" -m -s /bin/zsh robertg
 
 sudo -iu robertg mkdir -p -m 0700 .ssh
 echo "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDKHz85P9rJczwjgMjJu47/iLXBxtfqoSlHXjEnT4ZuDAAAABHNzaDo= robertg@macbook.local" >~robertg/.ssh/authorized_keys
 chmod 0400 ~robertg/.ssh/authorized_keys
 
-echo "robertg ALL=(ALL:ALL) NOPASSWD: ALL" >/etc/sudoers.d/robertg
+# No need for `wheel` or `sudo` group membership as explicitly specified below.
+cat >/etc/sudoers.d/robertg <<'EOF'
+Defaults:robertg !requiretty, env_keep += "http_proxy https_proxy ftp_proxy all_proxy no_proxy SSH_AGENT_PID SSH_AUTH_SOCK"
+robertg ALL=(ALL:ALL) NOPASSWD: ALL
+EOF
 chmod 0440 /etc/sudoers.d/robertg
 visudo -c
 
